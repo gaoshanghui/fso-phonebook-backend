9b1926dc6f57760272828a7f93022b9d61ef372c
Gao Shanghui <shanghui0109@gmail.com>
2021-03-19 20:11:32 +0900

    add step 1 to 6

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+function generateId() {
+  const id = Math.floor(Math.random() * 10000);
+  console.log("New item id is: ", id);
+  return id;
+}
+
+let persons = [
+  {
+    id: 1,
+    name: "Atro Hellas",
+    number: "040-1234567"
+  },
+  {
+    id: 2,
+    name: "aaa bbbb",
+    number: "010-1234567"
+  },
+  {
+    id: 3,
+    name: "ccc ssss",
+    number: "011-1230000"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const requestId = Number(request.params.id);
+  const person = persons.filter(person => person.id === requestId);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const requestId = Number(request.params.id);
+  persons = persons.filter(person => person.id !== requestId);
+  
+  response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) return response.status(400).json({ error: 'name must be included.' });
+  if (!body.number) return response.status(400).json({ error: 'number must be included.' });
+  if (persons.find(person => person.name === body.name)) return response.status(400).json({ error: 'name must be unique.' });
+  
+  body.id = generateId();
+  persons = persons.concat(body);
+
+  response.json(body)
+});
+
+app.get('/info', (request, response) => {
+  const date = new Date();
+  const numberOfPersons = persons.length;
+
+  response.send(`<p>Phonebook has info for ${numberOfPersons} people</p><p>${date}</p>`);
+});
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
